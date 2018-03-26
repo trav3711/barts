397ca1c0c45a2c1bc2e0f9cb06564bde174a8bf8
Nat Welch <nat@natwelch.com>
2018-03-26 17:02:46 +0000

    Add main.go

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+)
+
+// main is the function executed when the binary is run.
+func main() {
+
+	// Default port to 8080, but also allows override from env vars.
+	port := "8080"
+	if fromEnv := os.Getenv("PORT"); fromEnv != "" {
+		port = fromEnv
+	}
+
+	// Router is what decides what paths do what.
+	r := chi.NewRouter()
+
+	// GET / -> function
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome!!!"))
+	})
+
+	log.Printf("Server listening on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
